7a20e44fd368158618680ac3343352f74f2c583f
evan <evan.keya@student.moringaschool.com>
2023-09-19 21:28:43 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+var num1 = 31;
+var num2 = 2;
+let multiply = (num1 * num2)
+var results = num1 * num2
+console.log(results)
+
+var minNo = 1
+var maxNo = 100
+let random =Math.floor(Math.random() * (maxNo - minNo + 1)) + minNo;
+console.log(random)
+
+const num3 = 16;
+const num4 = 4;
+let mod = (num3/num4)
+console.log(mod)
+
+Math.max(2,6,1,8,20,14)
+let max = 20
+console.log(max)
+
+
+
